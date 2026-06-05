a71c094587736b75fe99f42b9aa70f4161ad6c76
Henry Euloge <henrynomo68@gamil.com>
2026-06-05 08:37:19 +0200

    test du samedi

--- front/src/utils/apiClient.ts
+++ front/src/utils/apiClient.ts
@@ -2,7 +2,7 @@ import axios from "axios"
 
 
 const apiClient = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || "https://hydrotech-backend-no5n.onrender.com",
+    baseURL: import.meta.env.VITE_API_URL || "https://hydrotech-project-backend.onrender.com",
     withCredentials: true
 })
 
